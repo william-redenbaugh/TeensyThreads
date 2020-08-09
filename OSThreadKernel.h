12ea5d7ebbdf058fd11fee4d2dd6e1992974af1a
William Redenbaugh <wredenba@uci.edu>
2020-08-08 21:55:31 -0700

    Completed Mutex integration without suspending threads

--- OSThreadKernel.h
+++ OSThreadKernel.h
@@ -347,6 +347,35 @@ extern "C" int enter_sleep(int ms);
 // This endif is for checking if we are using the Teensy4 IMXRT board
 #endif
 
+/*
+* @brief The different states that one can get from the mutex
+*/
+enum MutexLockState_t{
+  MUTEX_UNLOCKED, 
+  MUTEX_LOCKED
+};
+
+enum MutexLockReturnStatus{
+  MUTEX_ACQUIRE_SUCESS  = 1, 
+  MUTEX_ACQUIRE_FAIL    = 0
+};
+
+/*
+* @brief Object Reference to control our mutexes 
+* @notes So we can take care of our mutex stuff
+*/
+class MutexLock{
+  public: 
+    MutexLockState_t getState(void);
+    MutexLockReturnStatus lock(int timeout_ms);
+    MutexLockReturnStatus tryLock(void);
+    
+    void lockWaitIndefinite(void);
+    void unlock(void);
+
+  private: 
+    volatile MutexLockState_t state = MUTEX_UNLOCKED;
+};
 
 class Threads {
 public:
